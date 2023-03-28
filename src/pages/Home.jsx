adbdbbd6b0975485dd8dc6887f2064207ee5101a
anis <medanis.lakhdar@gmail.com>
2023-03-28 14:21:09 +0200

    refactor and optimize code , data fetch, use cache

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,58 @@
+import { useState, useEffect } from 'react';
 import bannerImg from '../assets/bannerIMG.jpg';
 import Card from '../components/Card';
 import '../styles/home.css';
-import logementList from '../datas/logements.json';
 import Banner from '../components/Banner';
 
 const Home = () => {
+  const [logements, setLogements] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const cacheKey = 'logementsData';
+    const cachedData = sessionStorage.getItem(cacheKey);
+
+    if (cachedData) {
+      setLogements(JSON.parse(cachedData));
+      setIsLoading(false);
+    } else {
+      fetch(`${process.env.PUBLIC_URL}/logements.json`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("La réponse du réseau n'est pas valide.");
+        })
+        .then(data => {
+          setLogements(data);
+          setIsLoading(false);
+          sessionStorage.setItem(cacheKey, JSON.stringify(data));
+        })
+        .catch(error => {
+          setIsLoading(false);
+          setError("Erreur lors du chargement des logements.");
+        });
+    }
+  }, []);
+
+  if (isLoading) {
+     return <div>chargement en cours...</div>;
+  }
+
+  if (error) {
+    return <div>Une erreur est survenue : {error}</div>;
+  }
+  
   return (
     <div>
       <Banner title="Chez vous, partout et ailleurs" image={bannerImg} />
       <div className="logements-list">
-        {logementList.map((logement) => (
+        {logements.map((logement) => (
             <Card
               key={logement.id}
-              id = {logement.id}
-              title={logement.title}
-              cover={logement.cover}
+              id={logement.id}
+              data={logement}
             />
         ))}
       </div>
