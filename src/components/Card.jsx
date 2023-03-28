b95ce3f911968652eb2ee2b60a9581e50142fd62
anis <>
2023-03-28 14:21:09 +0200

    refactor and optimize code , data fetch, use cache

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import '../styles/card.css';
 
-const Card = ({id, title, cover}) => {
+const Card = ({id, data}) => {
     return (
         <div className="card">
             <Link to={`/logement/${id}`} key={id}>
-                <img className='card__img' src={cover} alt={title}/>
+                <img className='card__img' src={data.cover} alt={data.title}/>
                 <div className='card__bg'></div>
                 <div className="card__content">
-                    <h3>{title}</h3>
+                    <h3>{data.title}</h3>
                 </div>
             </Link>
         </div>
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,13 +1,34 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Carousel from '../components/Carousel';
-import logements from '../datas/logements.json';
-
 
 const Logement = () => {
-  const {logementId} = useParams();
- 
-  const logement = logements.find((logement) => logement.id === logementId);
-  console.log(logement.pictures);
+  const { logementId } = useParams();
+  const [logement, setLogement] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const cacheKey = 'logementsData';
+    const cachedLogements = JSON.parse(sessionStorage.getItem(cacheKey)) || [];
+    
+    const logement = cachedLogements.find(l => l.id === logementId);
+    if (logement) {
+      setLogement(logement);
+      setIsLoading(false);
+    } else {
+      setError("Le logement n'a pas été trouvé.");
+      setIsLoading(false);
+    }
+  }, [logementId]);
+
+  if (isLoading) {
+    return <div>Chargement en cours...</div>;
+  }
+
+  if (error) {
+    return <div>Une erreur est survenue : {error}</div>;
+  }
 
   return (
     <div className="logement-details">
